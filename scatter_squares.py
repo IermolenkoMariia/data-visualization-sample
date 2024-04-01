ecce7dca58ce117b95672205038165f9a6e6be8a
Mariia Iermolenko <iermolenko.mariia@gmail.com>
2024-04-01 17:10:38 +0300

    use a colormap and automatically save the plot

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 ax.axis([0, 1100, 0, 1100000])
 
@@ -14,4 +14,4 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')

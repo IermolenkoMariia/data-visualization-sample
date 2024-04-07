8fb00d5fea1944ee7e490e65a5a128c22fce10f3
Mariia Iermolenko <iermolenko.mariia@gmail.com>
2024-04-07 19:33:58 +0300

    start and end points

--- rw_visual.py
+++ rw_visual.py
@@ -18,6 +18,21 @@ while True:
         s=15
     )
 
+    ax.scatter(
+        0, 
+        0, 
+        c='green', 
+        edgecolors='none', 
+        s=100
+    )
+    ax.scatter(
+        rw.x_values[-1], 
+        rw.y_values[-1], 
+        c='red', 
+        edgecolors='none', 
+        s=100
+    )
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
